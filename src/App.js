f2bdb839ef65ea53a6516f8abd884f02fc41e8c6
danielmdavis <>
2021-02-23 15:36:18 -0500

    first pass

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Logo from './GBTU_LOGO_600.png';
+import Banner from './genericprotestingfade.png';
+import Button from '@material-ui/core/Button';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <nav class="top-bar" id="nav-menu">
+
+        <div class="top-bar-left">
+            <ul class="vertical medium-horizontal dropdown menu" data-dropdown-menu>
+              <img className="bar-logo" src={Logo} />
+              <Button className="bar-button" variant="outlined" size="large">Know Your Rights</Button>
+              <Button className="bar-button" variant="outlined" size="large">Organize with Us</Button>
+              <Button className="bar-button" variant="outlined" size="large">About</Button>
+              <Button className="bar-button" variant="outlined" size="large">En Español</Button>
+              <ul class="submenu menu vertical medium-horizontal" data-submenu></ul>
+            </ul>
+        </div>
+
+        <div class="top-bar-right">
+            <ul class="vertical medium-horizontal dropdown menu" data-dropdown-menu>
+                    <ul class="submenu menu vertical medium-horizontal" data-submenu></ul>
+            </ul>
+        </div>
+
+      </nav>
+      <img className="banner-logo" src={Banner}/>
+      <h1 className='title'> Something inspiring.</h1>
+      <br/>
+      <h2 className='subtitle'>A shared problem calls for a collective response.</h2>
+      <br/><br/>
+      <div className='main-box'><div className='main-text'>
+          The Greater Boston Tenants Union is a collaborative of tenants across the greater boston area who have chosen to remain independent of nonprofits, big foundations, and government funding in order to build power that is responsive to and led by tenants. We are committed to base building and building leadership from the poorest. Our work is anti-capitalist, in order to resist the power of real estate capital that destroys our homes and our communities. Our work is anti-racist, in order to fight the gentrification and violence of the housing market, which affects the tenants of Boston unequally. COVID-19 has deepened Boston’s long standing tenant’s rights crisis. On October 17th, the governor and democratic and republican legislators alike allowed the statewide eviction moratorium to expire, all but ensuring a wave of evictions as winter sets in and COVID-19 cases spike. We are committed to organizing and building independent, tenant-led power to push back against that wave.
+      </div></div>
+      <img className="bar-logo" src={Logo}/>
+      <br/><br/>
+      <br/><br/>
+      <br/><br/>
+      <h2 className='subtitle'>We know there's a problem. Heres how we fight it.</h2>
+      <br/><br/>
+      <div className='main-box'><div className='main-text'>
+        The Greater Boston Tenants Union is a collaborative of tenants across the greater boston area who have chosen to remain independent of nonprofits, big foundations, and government funding in order to build power that is responsive to and led by tenants. We are committed to base building and building leadership from the poorest. Our work is anti-capitalist, in order to resist the power of real estate capital that destroys our homes and our communities. Our work is anti-racist, in order to fight the gentrification and violence of the housing market, which affects the tenants of Boston unequally. COVID-19 has deepened Boston’s long standing tenant’s rights crisis. On October 17th, the governor and democratic and republican legislators alike allowed the statewide eviction moratorium to expire, all but ensuring a wave of evictions as winter sets in and COVID-19 cases spike. We are committed to organizing and building independent, tenant-led power to push back against that wave.
+      </div></div>
+      <img className="bar-logo" src={Logo}/>
+      <br/><br/>
+      <br/><br/>
+      <br/><br/>
+      <h3 className='alt-subtitle'>Ready to get involved? Become a member today.</h3>
+      <br/><br/>
+
     </div>
   );
 }
